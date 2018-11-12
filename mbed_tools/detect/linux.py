6d20c1ac8b62cbb6db76f93f9e5632a56cd975e7
Brian Daniels <brianddaniels@gmail.com>
2018-11-12 16:33:46 -0600

    Adding Linux implementation of UsbDevice

--- mbed_tools/detect/linux.py
+++ mbed_tools/detect/linux.py
@@ -19,14 +19,20 @@ import re
 from os.path import join, isdir, dirname, abspath
 import os
 
-from .lstools_base import MbedLsToolsBase
+from . import UsbDevice
+from mbed_tools.utils import _run_cli_process
 
 import logging
-logger = logging.getLogger("mbedls.lstools_linux")
+logger = logging.getLogger("mbed_tools.detect.linux")
 logger.addHandler(logging.NullHandler())
 del logging
 
-SYSFS_BLOCK_DEVICE_PATH = '/sys/class/block'
+_SYSFS_BLOCK_DEVICE_PATH = '/sys/class/block'
+_NAME_LINK_PATTERN = re.compile(
+    r'(pci|usb)-[0-9a-zA-Z:_-]*_(?P<usbid>[0-9a-zA-Z]*)-.*$')
+_MOUNT_MEDIA_PATTERN = re.compile(
+    r'(?P<dev>(/[^/ ]*)+) on (?P<dir>(/[^/ ]*)+) ')
+_USB_DEVICE_PATTERN = re.compile(r'^[0-9]+-[0-9]+[^:\s]*$')
 
 def _readlink(link):
     content = os.readlink(link)
@@ -35,126 +41,146 @@ def _readlink(link):
     else:
         return content
 
-
-class MbedLsToolsLinuxGeneric(MbedLsToolsBase):
-    """ mbed-enabled platform for Linux with udev
+def _hex_ids(dev_list):
+    """! Build a USBID map for a device list
+    @param disk_list List of disks in a system with USBID decoration
+    @return Returns map USBID -> device file in /dev
+    @details Uses regular expressions to get a USBID (TargeTIDs) a "by-id"
+      symbolic link
     """
-    def __init__(self, **kwargs):
-        """! ctor
-        """
-        MbedLsToolsBase.__init__(self, **kwargs)
-        self.nlp = re.compile(
-            r'(pci|usb)-[0-9a-zA-Z:_-]*_(?P<usbid>[0-9a-zA-Z]*)-.*$')
-        self.mmp = re.compile(
-            r'(?P<dev>(/[^/ ]*)+) on (?P<dir>(/[^/ ]*)+) ')
-        self.udp = re.compile(r'^[0-9]+-[0-9]+[^:\s]*$')
+    logger.debug("Converting device list %r", dev_list)
+    for dl in dev_list:
+        match = _NAME_LINK_PATTERN.search(dl)
+        if match:
+            yield match.group("usbid"), _readlink(dl)
 
-    def find_candidates(self):
-        disk_ids = self._dev_by_id('disk')
-        serial_ids = self._dev_by_id('serial')
-        mount_ids = dict(self._fat_mounts())
-        usb_info = self._sysfs_block_devices(disk_ids.values())
-        logger.debug("Mount mapping %r", mount_ids)
+def _dev_by_id(device_type):
+    """! Get a dict, USBID -> device, for a device class
+    @param device_type The type of devices to search. For exmaple, "serial"
+      looks for all serial devices connected to this computer
+    @return A dict: Device USBID -> device file in /dev
+    """
+    dir = join("/dev", device_type, "by-id")
+    if isdir(dir):
+        to_ret = dict(_hex_ids([join(dir, f) for f in os.listdir(dir)]))
+        logger.debug("Found %s devices by id %r", device_type, to_ret)
+        return to_ret
+    else:
+        logger.error("Could not get %s devices by id. "
+                     "This could be because your Linux distribution "
+                     "does not use udev, or does not create /dev/%s/by-id "
+                     "symlinks. Please submit an issue to github.com/"
+                     "armmbed/mbed-ls.", device_type, device_type)
+        return {}
 
-        return [
-            {
-                'mount_point' : mount_ids.get(disk_dev),
-                'serial_port' : serial_ids.get(disk_uuid),
-                'target_id_usb_id' : disk_uuid,
-                'vendor_id': usb_info.get(disk_dev, {}).get('vendor_id'),
-                'product_id': usb_info.get(disk_dev, {}).get('product_id')
-            } for disk_uuid, disk_dev in disk_ids.items()
-        ]
+def _fat_mounts():
+    """! Lists mounted devices with vfat file system (potential mbeds)
+    @result Returns list of all mounted vfat devices
+    @details Uses Linux shell command: 'mount'
+    """
+    _stdout, _, retval = _run_cli_process('mount')
+    if not retval:
+        for line in _stdout.splitlines():
+            if b'vfat' in line:
+                match = _MOUNT_MEDIA_PATTERN.search(line.decode('utf-8'))
+                if match:
+                    yield match.group("dev"), match.group("dir")
 
-    def _dev_by_id(self, device_type):
-        """! Get a dict, USBID -> device, for a device class
-        @param device_type The type of devices to search. For exmaple, "serial"
-          looks for all serial devices connected to this computer
-        @return A dict: Device USBID -> device file in /dev
-        """
-        dir = join("/dev", device_type, "by-id")
-        if isdir(dir):
-            to_ret = dict(self._hex_ids([join(dir, f) for f in os.listdir(dir)]))
-            logger.debug("Found %s devices by id %r", device_type, to_ret)
-            return to_ret
-        else:
-            logger.error("Could not get %s devices by id. "
-                         "This could be because your Linux distribution "
-                         "does not use udev, or does not create /dev/%s/by-id "
-                         "symlinks. Please submit an issue to github.com/"
-                         "armmbed/mbed-ls.", device_type, device_type)
-            return {}
+def _sysfs_block_devices(block_devices):
+    device_names = { os.path.basename(d): d  for d in block_devices }
+    sysfs_block_devices = set(os.listdir(_SYSFS_BLOCK_DEVICE_PATH))
+    common_device_names = sysfs_block_devices.intersection(set(device_names.keys()))
+    result = {}
 
-    def _fat_mounts(self):
-        """! Lists mounted devices with vfat file system (potential mbeds)
-        @result Returns list of all mounted vfat devices
-        @details Uses Linux shell command: 'mount'
-        """
-        _stdout, _, retval = self._run_cli_process('mount')
-        if not retval:
-            for line in _stdout.splitlines():
-                if b'vfat' in line:
-                    match = self.mmp.search(line.decode('utf-8'))
-                    if match:
-                        yield match.group("dev"), match.group("dir")
+    for common_device_name in common_device_names:
+        sysfs_path = os.path.join(_SYSFS_BLOCK_DEVICE_PATH, common_device_name)
+        full_sysfs_path = os.readlink(sysfs_path)
+        path_parts = full_sysfs_path.split('/')
 
-    def _hex_ids(self, dev_list):
-        """! Build a USBID map for a device list
-        @param disk_list List of disks in a system with USBID decoration
-        @return Returns map USBID -> device file in /dev
-        @details Uses regular expressions to get a USBID (TargeTIDs) a "by-id"
-          symbolic link
-        """
-        logger.debug("Converting device list %r", dev_list)
-        for dl in dev_list:
-            match = self.nlp.search(dl)
-            if match:
-                yield match.group("usbid"), _readlink(dl)
+        end_index = None
+        for index, part in enumerate(path_parts):
+            if _USB_DEVICE_PATTERN.search(part):
+                end_index = index
 
-    def _sysfs_block_devices(self, block_devices):
-        device_names = { os.path.basename(d): d  for d in block_devices }
-        sysfs_block_devices = set(os.listdir(SYSFS_BLOCK_DEVICE_PATH))
-        common_device_names = sysfs_block_devices.intersection(set(device_names.keys()))
-        result = {}
+        if end_index == None:
+            logger.debug('Did not find suitable usb folder for usb info: %s', full_sysfs_path)
+            continue
 
-        for common_device_name in common_device_names:
-            sysfs_path = os.path.join(SYSFS_BLOCK_DEVICE_PATH, common_device_name)
-            full_sysfs_path = os.readlink(sysfs_path)
-            path_parts = full_sysfs_path.split('/')
+        usb_info_rel_path = path_parts[:end_index + 1]
+        usb_info_path = os.path.join(_SYSFS_BLOCK_DEVICE_PATH, os.sep.join(usb_info_rel_path))
 
-            end_index = None
-            for index, part in enumerate(path_parts):
-                if self.udp.search(part):
-                    end_index = index
+        vendor_id = None
+        product_id = None
 
-            if end_index == None:
-                logger.debug('Did not find suitable usb folder for usb info: %s', full_sysfs_path)
-                continue
+        vendor_id_file_paths = os.path.join(usb_info_path, 'idVendor')
+        product_id_file_paths = os.path.join(usb_info_path, 'idProduct')
 
-            usb_info_rel_path = path_parts[:end_index + 1]
-            usb_info_path = os.path.join(SYSFS_BLOCK_DEVICE_PATH, os.sep.join(usb_info_rel_path))
+        try:
+            with open(vendor_id_file_paths, 'r') as vendor_file:
+                vendor_id = vendor_file.read().strip()
+        except OSError as e:
+            logger.debug('Failed to read vendor id file %s weith error:', vendor_id_file_paths, e)
 
-            vendor_id = None
-            product_id = None
+        try:
+            with open(product_id_file_paths, 'r') as product_file:
+                product_id = product_file.read().strip()
+        except OSError as e:
+            logger.debug('Failed to read product id file %s weith error:', product_id_file_paths, e)
 
-            vendor_id_file_paths = os.path.join(usb_info_path, 'idVendor')
-            product_id_file_paths = os.path.join(usb_info_path, 'idProduct')
+        result[device_names[common_device_name]] = {
+            'vendor_id': vendor_id,
+            'product_id': product_id
+        }
 
-            try:
-                with open(vendor_id_file_paths, 'r') as vendor_file:
-                    vendor_id = vendor_file.read().strip()
-            except OSError as e:
-                logger.debug('Failed to read vendor id file %s weith error:', vendor_id_file_paths, e)
+    return result
 
-            try:
-                with open(product_id_file_paths, 'r') as product_file:
-                    product_id = product_file.read().strip()
-            except OSError as e:
-                logger.debug('Failed to read product id file %s weith error:', product_id_file_paths, e)
 
-            result[device_names[common_device_name]] = {
-                'vendor_id': vendor_id,
-                'product_id': product_id
-            }
+class LinuxUsbDevice(UsbDevice):
+    '''Linux implementation for the UsbDevice class'''
 
-        return result
+    def __init__(self, usb_id, vendor_id, product_id):
+        self._usb_id = usb_id
+        self._vendor_id = vendor_id
+        self._product_id = product_id
+
+        if self._vendor_id is None or self._product_id is None:
+            logger.warning('Received incomplete USB data', {
+                'usb_id': self._usb_id,
+                'vendor_id': self._vendor_id,
+                'product_id': self._product_id
+            })
+
+    @property
+    def usb_id(self):
+        return self._usb_id
+
+    @property
+    def vendor_id(self):
+        return self._vendor_id
+
+    @property
+    def product_id(self):
+        return self._product_id
+
+    def mount_point(self):
+        mount_point = None
+        disk_name = _dev_by_id('disk').get(self._usb_id)
+        if disk_name:
+            mount_point = dict(_fat_mounts()).get(disk_name)
+        return mount_point
+
+    def serial_port(self):
+        return _dev_by_id('serial').get(self._usb_id)
+
+
+def find_usb_devices():
+    disks = _dev_by_id('disk')
+    usb_info = _sysfs_block_devices(disks.values())
+
+    return [
+        LinuxUsbDevice(
+            usb_id,
+            usb_info.get(disk_name, {}).get('vendor_id'),
+            usb_info.get(disk_name, {}).get('product_id')
+        ) for usb_id, disk_name in disks.items()
+    ]
