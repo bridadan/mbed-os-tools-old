ea92d57291ceffef61e0a84255bc198bdcf90c66
Brian Daniels <brianddaniels@gmail.com>
2018-11-12 16:32:48 -0600

    Adding usb details to UsbDevice class

--- mbed_tools/detect/usb_device.py
+++ mbed_tools/detect/usb_device.py
@@ -8,20 +8,47 @@ class UsbDevice(object):
     # ABC class is not supported in Python 2.7.x
     __metaclass__ = ABCMeta
 
+    @property
+    @abstractmethod
+    def usb_id(self):
+        '''Return the USB id of the UsbDevice
+
+        @return A string of the usb id
+        '''
+        raise NotImplementedError
+
+    @property
+    @abstractmethod
+    def vendor_id(self):
+        '''Return the USB vendor id of the UsbDevice
+
+        @return A string of the vendor id
+        '''
+        raise NotImplementedError
+
+    @property
+    @abstractmethod
+    def product_id(self):
+        '''Return the USB product id of the UsbDevice
+
+        @return A string of the product id
+        '''
+        raise NotImplementedError
+
     @abstractmethod
     def mount_point(self):
-    '''Query the system for the mount point associated with the UsbDevice
+        '''Query the system for the mount point associated with the UsbDevice
 
-    @return If mounted, a string of the mount point.
-    Otherwise, None.
-    '''
+        @return If mounted, a string of the mount point.
+        Otherwise, None.
+        '''
         raise NotImplementedError
 
     @abstractmethod
     def serial_port(self):
-    '''Query the system for the serial port associated with the UsbDevice
+        '''Query the system for the serial port associated with the UsbDevice
 
-    @return If a serial port exists, a string representing the serial port.
-    Otherwise, None.
-    '''
+        @return If a serial port exists, a string representing the serial port.
+        Otherwise, None.
+        '''
         raise NotImplementedError
